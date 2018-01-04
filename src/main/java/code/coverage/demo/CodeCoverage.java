8fd0f991c0815e173c740bc3ffee8814d21574b5
mohan <y.mohansai0825@gmail.com>
2018-01-04 21:58:46 +0530

    this code is for code coverage

---
+++ src/main/java/code/coverage/demo/CodeCoverage.java
@@ -0,0 +1,18 @@
+package code.coverage.demo;
+
+public class CodeCoverage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		start();
+	}
+public static String start(){
+	System.out.println("start");
+	return "start";
+}
+public static String stop(){
+	System.out.println("stop");
+	return "stop";
+}
+}
